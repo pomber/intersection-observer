bfacebc207cfa5092bc320151b513757aaa7d07a
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-10-18 14:59:14 +0000

    Add values

--- src/lib/CodeCallback.svelte
+++ src/lib/CodeCallback.svelte
@@ -68,13 +68,61 @@
 	const [firstLine, ...middle] = lines
 	const lastLine = middle.pop()
 
+	// add --i and trace class
 	let i = 0
 	const xlines = middle.map((line) =>
 		line.map(([content, color]) => {
 			if (content.trim().length < 3) return { content, color }
-			return { content, color, i: i++, trace: 'trace' }
+			return { content, color, i: i++, trace: true }
 		})
 	)
+
+	$: callback = xlines.map((line, i) => {
+		if (i === 0) {
+			return [
+				...line,
+				{ content: ' ' },
+				{ content: $entries.length, i: 4, trace: true, value: true },
+			]
+		}
+		if (i === 2) {
+			return [
+				...line,
+				{ content: ' ' },
+				...$entries.map((entry) => ({
+					content: entry.targetId,
+					i: 10,
+					trace: true,
+					value: true,
+				})),
+			]
+		}
+		if (i === 3) {
+			return [
+				...line,
+				{ content: ' ' },
+				...$entries.map((entry) => ({
+					content: entry.isIntersecting,
+					i: 13,
+					trace: true,
+					value: true,
+				})),
+			]
+		}
+		if (i === 4) {
+			return [
+				...line,
+				{ content: ' ' },
+				...$entries.map((entry) => ({
+					content: entry.intersectionRatio.toFixed(3),
+					i: 16,
+					trace: true,
+					value: true,
+				})),
+			]
+		}
+		return line
+	})
 
 	let isOff = false
 	entries.subscribe(() => {
@@ -87,11 +135,12 @@
 
 <div class={isOff ? 'off' : 'on'}>
 	<CodeTokens tokens={firstLine} br />
-	{#each xlines as line, i}
+	{#each callback as line}
 		<div style:white-space="pre">
-			{#each line as { content, color, trace, i }}<span
+			{#each line as { content, color, trace, value, i }}<span
 					style:color
 					class:trace
+					class:value
 					style={i != null ? `--i: ${i}` : ''}>{content}</span
 				>{/each}
 		</div>
@@ -103,11 +152,32 @@
 	.trace {
 		border-radius: 0.25rem;
 	}
+	.value {
+		border-radius: 0.25rem;
+		padding: 0.1rem 0.2rem;
+		margin: 0 -0.2rem 0 0;
+		background: hsl(261deg 55% 42%);
+		color: #e1e4e8;
+		position: relative;
+		right: 2px;
+		user-select: none;
+		transition: opacity 0.1s;
+	}
 
 	.on .trace {
 		filter: brightness(1);
 		animation: focus 0.3s;
 		animation-delay: calc(0.02s * var(--i));
+	}
+
+	.on .trace.value {
+		opacity: 0;
+		animation: focus 0.3s, show 0.3s;
+		animation-delay: calc(0.02s * var(--i));
+		animation-fill-mode: forwards;
+	}
+	.off .value {
+		opacity: 0;
 	}
 	@keyframes focus {
 		0% {

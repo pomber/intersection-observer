0a9437401fcc02f0799730f9075abb1622b52870
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-10-19 14:31:03 +0000

    Threshold buttons

--- src/lib/CodeThreshold.svelte
+++ src/lib/CodeThreshold.svelte
@@ -1,4 +1,5 @@
 <script>
+	import CodeThresholdButtons from './CodeThresholdButtons.svelte'
 	import { controls, selection } from './stores'
 
 	// @ts-ignore
@@ -18,11 +19,52 @@
 	}
 
 	$: selected = $selection === 'threshold'
+
+	/**
+	 * @param {number} i
+	 */
+	function decrease(i) {
+		if ($controls.thresholds[i] < 0.1) return
+		controls.update((c) => {
+			c.thresholds[i] = c.thresholds[i] - 0.1
+			return c
+		})
+	}
+
+	/**
+	 * @param {number} i
+	 */
+	function increase(i) {
+		if ($controls.thresholds[i] > 0.9) return
+		controls.update((c) => {
+			c.thresholds[i] = c.thresholds[i] + 0.1
+			return c
+		})
+	}
+
+	/**
+	 * @param {number} i
+	 */
+	function remove(i) {
+		if ($controls.thresholds.length === 1) return
+		controls.update((c) => {
+			c.thresholds.splice(i, 1)
+			return c
+		})
+	}
+
+	/**
+	 * @param {number} value
+	 */
+	function round(value) {
+		return Math.round(value * 100) / 100
+	}
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <div
 	tabindex="0"
+	class:selected
 	data-selection={$selection}
 	on:click={setSelection}
 	on:keydown={setSelection}
@@ -34,7 +76,15 @@
 	<span class="fg">
 		<span style:white-space="pre" style:color="#E1E4E8">{'  threshold: ['}</span
 		><span style:color="#9ECBFF"
-			>{$controls.thresholds.map((x) => x.toFixed(2)).join(', ')}</span
+			>{#each $controls.thresholds as t, i}<span class="number"
+					>{t.toFixed(2)}<span class="buttons"
+						><CodeThresholdButtons
+							decrease={() => decrease(i)}
+							remove={() => remove(i)}
+							increase={() => increase(i)}
+						/></span
+					></span
+				>{#if i < $controls.thresholds.length - 1},{/if}{/each}</span
 		><span style:color="#E1E4E8">{']'}</span></span
 	>
 </div>
@@ -60,8 +110,35 @@
 		transition: opacity 0.2s;
 		user-select: none;
 	}
+	.number {
+		position: relative;
+		display: inline-block;
+		min-width: 5ch;
+		text-align: center;
+		transition: min-width 0.2s;
+	}
+	.selected .number {
+		min-width: 7ch;
+	}
+
+	.buttons {
+		position: absolute;
+		opacity: 0;
+		transform: translate(-50%, 50%);
+		left: 50%;
+		top: 0;
+		transition: opacity 0.2s, transform 0.2s ease-out;
+		user-select: none;
+		height: 22px;
+	}
+
+	.selected .buttons {
+		transform: translate(-50%, 100%);
+		display: inline-block;
+		opacity: 1;
+	}
 	div:hover .bg,
-	div[data-selection='threshold'] .bg {
+	.selected .bg {
 		opacity: 0.7;
 	}
 </style>
---
+++ src/lib/CodeThresholdButtons.svelte
@@ -0,0 +1,110 @@
+<script>
+	/**
+	 * @type {() => void}
+	 */
+	export let decrease
+	/**
+	 * @type {() => void}
+	 */
+	export let remove
+	/**
+	 * @type {() => void}
+	 */
+	export let increase
+</script>
+
+<span class="group">
+	<button on:click={decrease}
+		><svg
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M20 12H4"
+			/>
+		</svg></button
+	>
+	<button on:click={remove}
+		><svg
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M6 18L18 6M6 6l12 12"
+			/>
+		</svg></button
+	>
+	<button on:click={increase}
+		><svg
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+			/></svg
+		></button
+	>
+</span>
+
+<style>
+	.group {
+		display: inline-block;
+		font-size: 6px;
+		border-radius: 4px;
+		border: 1px solid hsl(261deg 55% 60%);
+		overflow: hidden;
+		background: hsl(215deg 28% 17%);
+		width: 60px;
+		height: 20px;
+		position: relative;
+	}
+	button {
+		display: inline-block;
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		border: none;
+		background: none;
+		color: white;
+		font-size: 10px;
+		margin: 0;
+		padding: 0;
+		cursor: pointer;
+		user-select: none;
+		transition: opacity 0.2s ease;
+	}
+	button:first-child {
+		left: 0;
+	}
+	button:nth-child(2) {
+		left: 20px;
+	}
+	button:last-child {
+		right: 0;
+	}
+	button:hover {
+		background: hsl(215deg 28% 30%);
+	}
+	svg {
+		width: 14px;
+		height: 14px;
+		display: block;
+		margin: 0 auto;
+		stroke: currentColor;
+		fill: currentColor;
+	}
+</style>
